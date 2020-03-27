c46d012f8a38e325db510a9c80595ea55f6a555e
JoLeaper <joleap123@gmail.com>
2020-03-27 13:40:13 -0700

    added first input, button, and sections containing inputs/madlibs

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1. Get elements from their ids.
+// 2. Create input variables for ALL variables.
+// 3. Create madlib container.
+// 4. Create input container.
+// 5. Create button.
+// 6. Add event listener to button.
+// 7. Create function that hides input container with mab libs.
+
+const buttonPress = document.getElementById('button');
+const input1 = document.getElementById('number1');
+
+addEventListener('click', () => {
+
+});
